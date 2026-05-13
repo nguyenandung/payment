8514bbb0f16467c3b4575c0799e57e25bf4d20bf
mac <mac@MacBookPro.lan>
2026-05-14 00:06:57 +0700

    khoi tao

---
+++ src/main/java/com/engineerpro/rest/example/model/User.java
@@ -0,0 +1,34 @@
+package com.engineerpro.rest.example.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private int id;
+
+@Column(name = "name", length = 150)
+  private String name;
+
+  
+  @Column(name = "balance")
+  private int balance;
+}
